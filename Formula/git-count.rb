2dcc01d4c1fe47058d5d005bb7924c6a6810f3e1
mironal <miro.gunp@gmail.com>
2013-11-03 10:09:28 +0900

    git-count 1.1.0 に対応

--- Formula/git-count.rb
+++ Formula/git-count.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GitCount < Formula
   homepage 'https://github.com/neethouse/git-count'
-  url 'https://github.com/neethouse/git-count.git', :tag => '1.0.0'
+  url 'https://github.com/neethouse/git-count.git', :tag => '1.1.0'
 
   head 'https://github.com/neethouse/git-count.git', :branch => 'master'
 
