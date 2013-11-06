950cb484ff5256039bb23fee2d03d954aef45d4d
mironal <miro.gunp@gmail.com>
2013-11-06 17:11:55 +0900

    本家の homebrew に申請したFomuraと合わせた.
    merge はされなかったけどな！=͟͟͞͞( ∩ ՞ةڼ◔=͟͟͞͞) ੭ु⁾⁾

--- Formula/git-count.rb
+++ Formula/git-count.rb
@@ -1,13 +1,13 @@
 require 'formula'
 
 class GitCount < Formula
-  homepage 'https://github.com/neethouse/git-count'
-  url 'https://github.com/neethouse/git-count.git', :tag => '1.1.0'
 
-  head 'https://github.com/neethouse/git-count.git', :branch => 'master'
+  homepage 'https://github.com/neethouse/git-count'
+  url 'https://github.com/neethouse/git-count/archive/1.1.0.tar.gz'
+  sha1 '0a6581a2dc5ea1f04f3937d0ba5b71a8c4335be7'
 
   def install
-    bin.install('git-count')
+    bin.install 'git-count'
   end
 
 end
