ec01fa9bf10a851a575cd1f1f07b9c21cfa037c6
mironal <miro.gunp@gmail.com>
2013-12-29 13:29:10 +0900

    git-count 1.4.0

--- Formula/git-count.rb
+++ Formula/git-count.rb
@@ -3,8 +3,8 @@ require 'formula'
 class GitCount < Formula
 
   homepage 'https://github.com/neethouse/git-count'
-  url 'https://github.com/neethouse/git-count/archive/1.3.0.tar.gz'
-  sha1 'e2cddf87306385ea47d2caf49a50a18091f7f9b4'
+  url 'https://github.com/neethouse/git-count/archive/1.4.0.tar.gz'
+  sha1 '0eab3500c1699ce4b02f4dd9b2fa40f374f0efd2'
 
   def install
     bin.install 'git-count'
